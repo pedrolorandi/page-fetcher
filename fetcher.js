5c1b8f1a7e7592dea0b10b9aaae40bb122fe899a
Pedro Lorandi <hello@pedrolorandi.com>
2022-11-18 11:18:02 -0500

    Fix fetcher.js

--- fetcher.js
+++ fetcher.js
@@ -7,9 +7,9 @@ const filePath = process.argv[3];
 request(url, (error, response, body) => {
   fs.writeFile(filePath, body, (error) => {
     if (error) {
-      console.log('error:', error); // Print the error if one occurred
+      console.log('error:', error);
     }
     
-    console.log(`Downloaded and saved ${response.headers['content-length']} bytes to ${filePath}`)
-  })
+    console.log(`Downloaded and saved ${response.headers['content-length']} bytes to ${filePath}`);
+  });
 });
